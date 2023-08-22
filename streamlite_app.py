fd3e3e0fd154407932c4bd9de5f8ca0518b11657
Navani55 <142522364+Navani55@users.noreply.github.com>
2023-08-22 20:55:22 +0530

    Update streamlite_app.py
    added texts

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-
+streamlit.text('Omega 3 & Blueberry Oatmetal')
+streamlit.text('kale,spinach & Rocket smoothe')
+streamlit.text('Hard-Boiled Free-Range Egg ')
+streamlit.text('-----END----')
