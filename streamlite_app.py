cf8196a3c6454a001e30c2169ce780dc1a3c3bc5
Navani55 <142522364+Navani55@users.noreply.github.com>
2023-08-22 21:01:34 +0530

    Update streamlite_app.py
    Change Header from Braekfast menu to Breakfast Favourites 
    Add symbols to the items

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmetal')
-streamlit.text('kale,spinach & Rocket smoothe')
-streamlit.text('Hard-Boiled Free-Range Egg ')
-streamlit.text('-----END----')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmetal')
+streamlit.text('🥗 kale,spinach & Rocket smoothe')
+streamlit.text('🐔Hard-Boiled Free-Range Egg ')
+streamlit.text('🥑🍞 Avacado Toast')
