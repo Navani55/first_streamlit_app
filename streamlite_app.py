ea5396b66db60b169740d8ab5d3b6e9872811f9b
Navani55 <142522364+Navani55@users.noreply.github.com>
2023-08-28 23:09:18 +0530

    Update streamlite_app.py
    create a text box which acts as a variable

--- streamlite_app.py
+++ streamlite_app.py
@@ -22,9 +22,10 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruity Fruit Advice')
-
+fruit_choice=streamlit.text.input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # normalize the json 
