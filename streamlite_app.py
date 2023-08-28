2984ba1251db3f002d1a5c7154e4d1f889989559
Navani55 <142522364+Navani55@users.noreply.github.com>
2023-08-28 22:44:37 +0530

    Update streamlite_app.py
    normalise the Json and put it into a dataframe

--- streamlite_app.py
+++ streamlite_app.py
@@ -25,6 +25,11 @@ streamlit.header('Fruity Fruit Advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes data to screen
+
+# normalize the json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put the normalised json into a dataframe
+streamlit.dataframe(fruityvice_normalized)
 
 
